68f77c42eaa498266a452451871c1dd0838f9b96
Clement DUBREUIL <clement.dubreuil2020@campus-eni.fr>
2021-08-25 13:42:28 +0200

    ajout route pour ajouter un souhait en dur

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\WishRepository;
+use App\Entity\Wish;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,12 +27,28 @@ class WishController extends AbstractController
     /**
      * @Route("/wish/{id}", name="wish")
      */
-    public function wish($id, WishRepository $wishRepo): Response
+    public function wish(Wish $wish): Response
     {
-        $wish = $wishRepo->find($id);
-
         return $this->render('wish/wish.html.twig', [
             'wish' => $wish,
         ]);
+    }
+
+    /**
+     * @Route("/add-wish", name="addWish")
+     */
+    public function addWish(EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+        $wish->setTitle('Manger sainement');
+        $wish->setDescription('Il faut manger sainement pour être en bonne santé !');
+        $wish->setAuthor('Clément');
+        $wish->setIsPublished(true);
+        $wish->setDateCreated(new \DateTime());
+
+        $em->persist($wish);
+        $em->flush();
+
+        return $this->redirectToRoute('wishList');
     }
 }
